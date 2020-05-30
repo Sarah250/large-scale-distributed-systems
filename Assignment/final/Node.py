9fa0f2a413da1d6aced6d64260ac8d21259a1d9e
Sarah250 <Sarah250@github.com>
2020-05-30 14:44:24 +0100

    commit

--- Assignment/final/Node.py
+++ Assignment/final/Node.py
@@ -7,19 +7,29 @@ from Msg import Msg
 class Node:
     def __init__(self, K, T, neighbors, myNumber):
         self.T = T
+        self.N = 0
         self.vectorX = random.exponential(scale=1, size=K)
         self.neighbors = neighbors
-        self.N = 0
         self.oldX = self.vectorX
         self.nonews = 0
         self.converged = False
         self.myNumber = myNumber
+        #
         self.expectedResponses = []
-        self.dist = {}
+        self.dist2 = {}
+        self.dist = []
         self.numberOfResponses = 0
+        self.pending = []
 
     def setDist(self, dist):
-        self.dist = dist
+        self.dist2 = dist
+        for key in dist:
+            self.dist.append((key, dist[key]))
+        self.dist = sorted(self.dist, key=lambda x: x[1])
+
+    def calculatingExpectedResponses_v1(self):
+        for j in self.dist:
+            self.expectedResponses.append((j[0][1], j[0][0]))
 
     def addResponde(self, response):
         self.expectedResponses.append(response)
@@ -30,18 +40,21 @@ class Node:
         self.oldX = self.vectorX
         pending = []
 
-        for nei in self.neighbors:
-            self.expectedResponses.append((nei, self.myNumber))
-            disTime = self.dist[(self.myNumber, nei)]
+        self.calculatingExpectedResponses_v1()
+
+        for nei in self.dist:
+            disTime = nei[1]
             msg = Msg(self.vectorX)
-            event = ((self.myNumber, nei), msg, disTime + clock)
+            event = ((self.myNumber, nei[0][1]), msg, disTime + clock)
             pending.append(event)
+        print(f'{self.myNumber} -> {self.expectedResponses}')
         return pending
 
     def receive(self, msgX, src, clock):
         pending = []
-        # Round 2: recebe mensagem de um nodo
-        if len(self.expectedResponses) == 0:
+        # Round 2: recebe mensagem de um nodo qualquer!
+        if not ((src, self.myNumber) in self.expectedResponses):
+            print(f' Recebi 1 msg {self.myNumber} <- {src}')
             # oldX <- vectorX
             self.oldX = self.vectorX
             # vectorX da mensagem
@@ -51,30 +64,28 @@ class Node:
             # adiciono às mensagens esperadas
             self.expectedResponses.append((src, self.myNumber))
             msg = Msg(self.vectorX)
-            distTime = self.dist[(self.myNumber, src)]
+            distTime = self.dist2[(self.myNumber, src)]
             event = ((self.myNumber, src), msg, distTime + clock)
             pending.append(event)
             return pending
         # Round 3:
-        if len(self.expectedResponses) > 0:
+        elif (src, self.myNumber) in self.expectedResponses:
             # vectorX da mensagem
-            vectorX = msgX.vectorX
-            # se for a mensagem final
+            msg = msgX.vectorX
+            # se for a mensagem final -> Quando um vizinho recebe a resposta final
             if msgX.final:
+                #print(f'mensagem final de {src} para {self.myNumber}')
                 self.expectedResponses.remove((src, self.myNumber))
                 # oldX <- vectorX
                 self.oldX = self.vectorX
-
                 # calculate and update vectorX with the pontwiseMinimum
-                self.vectorX = np.minimum(self.vectorX, vectorX)
+                self.vectorX = np.minimum(self.vectorX, msg)
                 return None
             else:
-                print(f'{(src, self.myNumber)}')
-                print(f'{(self.expectedResponses)}')
                 self.expectedResponses.remove((src, self.myNumber))
                 self.oldX = self.vectorX
                 # calculate and update vectorX with the pontwiseMinimum
-                self.vectorX = np.minimum(self.vectorX, vectorX)
+                self.vectorX = np.minimum(self.vectorX, msg)
                 #
                 self.numberOfResponses += 1
                 #
@@ -90,9 +101,11 @@ class Node:
                     pending = []
 
                     for nei in self.neighbors:
+                        #print(f'Eu {self.myNumber} vou enviar mensagens finais a {nei}')
                         self.expectedResponses.append((nei, self.myNumber))
-                        disTime = self.dist[(self.myNumber, nei)]
+                        disTime = self.dist2[(self.myNumber, nei)]
                         msg = Msg(self.vectorX)
+                        msg.final = True
                         event = ((self.myNumber, nei), msg, disTime + clock)
                         pending.append(event)
                     return pending
--- Assignment/final/Simulator.py
+++ Assignment/final/Simulator.py
@@ -1,5 +1,6 @@
 import atexit
-
+import networkx as nx
+import matplotlib.pyplot as plt
 from Node import Node
 from connected import randomGraph
 from numpy import random
@@ -8,12 +9,13 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
 
-
-
 class Simulator:
 
     def __init__(self, K, T, numberOfNodes, percentage):
         self.graph = randomGraph(numberOfNodes)
+        plt.subplot(121)
+        nx.draw(self.graph, with_labels=True)
+        plt.show()
         self.nodes = list(self.graph.nodes)
         self.T = T
         self.K = K
@@ -36,13 +38,14 @@ class Simulator:
             localDist = {}
 
             for j in self.nodes[i].neighbors:
+                #time = random.randint(1, 10)
                 time = random.random()
                 self.dist[(i, j)] = time
-                localDist[(i,j)] = time
+                localDist[(i, j)] = time
             self.nodes[i].setDist(localDist)
 
-    def receiveMsg(self, src,dst, msg):
-        #print(f"Mensagem de {src} -> {dst}")
+    def receiveMsg(self, src, dst, msg):
+        # print(f"Mensagem de {src} -> {dst}")
         result = self.nodes[dst].receive(msg, src, self.time)
         if result != None:
             self.pending = self.pending + result
@@ -53,19 +56,20 @@ class Simulator:
         flag = True
         i = 0
         N = 0
+        list = []
 
         while i < len(self.nodes) and flag:
-            if self.nodes[i].converged:
-                N = self.nodes[i].estimating()
-                flag = False
+            #if self.nodes[i].converged:
+            list.append((self.nodes[i].myNumber,self.nodes[i].converged, self.nodes[i].estimating()))
+            # flag = False
             i += 1
-        print(f"Estimated value = {N}")
+        print(f"Estimated value = {list}")
 
     def timer(self):
         self.s.start()
         self.s.add_job(
             func=self.queueing,
-            trigger=IntervalTrigger(seconds=10),
+            trigger=IntervalTrigger(seconds=30),
             id='printing_time_job',
             name='Print time every 20 seconds',
             replace_existing=True
@@ -73,39 +77,52 @@ class Simulator:
         # Shut down the scheduler when exiting the app
         atexit.register(lambda: self.s.shutdown())
 
+    def updatePending(self):
+        pending = []
+        for p in self.pending:
+            pending.append((p[0], p[1], p[2] + self.time))
+        self.pending = pending
+
     def run_loop(self):
         i = 0
         self.timer()
+
         pending = self.nodes[i].handle(self.time)
         self.pending = self.pending + pending
         while len(self.pending) > 0:
             # 1. Minimum distances Nodes ((src, dst), time)
             result = sorted(self.pending, key=lambda x: x[2])
+            #print(result[0])
             src = result[0][0][0]
             dst = result[0][0][1]
-            time = result[0][2]
+            time2 = self.dist[(src,dst)]
             msg = result[0][1]
 
             # enviar mensagem:
-            self.sched.enter(time, 0, self.receiveMsg,argument=(src,dst,msg))
+            print(f"SCHED - {src} -> {dst} : {time2}")
+            self.sched.enter(time2, 1, self.receiveMsg, argument=(src, dst, msg))
             # blocking
             self.sched.run()
 
             # remove from pending
-            self.pending.remove(result[0])
+            if result[0] in self.pending:
+                self.pending.remove(result[0])
 
             # update time
-            self.time += time
-            #print(f'{i}. Clock = {self.time}')
-            #print('----------------------------------------------')
+            self.time += time2
+            # update self.pending
+            #self.updatePending()
+            # print(f'{i}. Clock = {self.time}')
+            # print('----------------------------------------------')
             i += 1
-            if i == self.numberOfNodes:
-                i = 0
-            pending = self.nodes[i].handle(self.time)
-            self.pending = self.pending + pending
-    
+            if i < self.numberOfNodes:
+                pending = self.nodes[i].handle(self.time+1)
+                self.pending = self.pending + pending
+        self.queueing()
+
 
 if __name__ == '__main__':
-    simulator = Simulator(10, 4, 10, 0.2)
+    simulator = Simulator(1000, 4, 10, 0.2)
     simulator.initialize()
     simulator.run_loop()
+
